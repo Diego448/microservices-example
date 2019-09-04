1437e4c17bd34c530b9226eaacbcfdd905a2ec51
Diego Serrano Gómez <diegosg448@gmail.com>
2019-09-04 01:03:10 -0500

    Adding pug template and updating code to use it

--- personas.js
+++ personas.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const pug = require('pug');
+const compiledFunction = pug.compileFile('template.pug');
 
 var directorio = {"personas": [{"nombre": "Erick"}, {"nombre": "Octavio"}, {"nombre": "Julio"}]};
 
+app.get('/', (req, res) => res.send(pug.renderFile('template.pug', {directorio: directorio})));
 app.get('/personas', (req, res) => res.json(directorio));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/personas/:nombre', function (req, res) {
+    directorio.personas.push({"nombre": req.params.nombre});
+    res.send(req.params.nombre + " successfully added to database");
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
